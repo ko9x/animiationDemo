51c16f7b1b4ef069f009909756888ba61e1054c1
Sean McGillis <slcidevice@yahoo.com>
2022-08-23 23:05:23 -0600

    Setup CircularProgress

--- components/CircularProgress.js
+++ components/CircularProgress.js
@@ -1,6 +1,14 @@
 import React, {FC} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Animated, Button} from 'react-native';
+import {
+  useAnimatedProps,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import Svg, {Circle} from 'react-native-svg';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 export function CircularProgress({
   radius,
@@ -10,12 +18,45 @@ export function CircularProgress({
 }) {
   const innerRadius = radius - strokeWidth / 2;
   const circumfrence = 2 * Math.PI * innerRadius;
+  const invertedCompletion = (100 - percentageComplete) / 100;
+
+  const theta = useSharedValue(2 * Math.PI * 1.001);
+  const animateTo = useDerivedValue(() => 2 * Math.PI * invertedCompletion);
+
+  const animatedProps = useAnimatedProps(() => {
+    console.log('thisRan', theta.value ); //@DEBUG
+    return {
+      strokeDashoffset: withTiming(theta.value * innerRadius, {
+        duration: 1500,
+      }),
+    };
+  });
+
+  console.log('animateTo.value', animateTo.value); //@DEBUG
+  console.log('theta.value', theta.value); //@DEBUG
 
   return (
     <View style={styles.container}>
       <Svg style={StyleSheet.absoluteFill}>
-        <Circle cx={radius} cy={radius} r={radius} fill={backgroundColor} />
+        <AnimatedCircle
+          animatedProps={animatedProps}
+          cx={radius}
+          cy={radius}x
+          fill={'transparent'}
+          r={innerRadius}
+          stroke={backgroundColor}
+          strokeDasharray={`${circumfrence} ${circumfrence}`}
+          strokeWidth={strokeWidth}
+        //   strokeDashoffset={2 * Math.PI * (innerRadius * 0.5)}
+          strokeLinecap="round"
+        />
       </Svg>
+      <Button
+        title='Animate'
+        onPress={() => {
+            theta.value = animateTo.value;
+        }}
+      />
     </View>
   );
 }
